afba6fe22ce1ac927d5d1cae04f34d119cf68719
GitHpotato <GitHpotato@users.noreply.github.com>
2019-08-10 20:36:56 +0800

    Update music.js

--- JS/music.js
+++ JS/music.js
@@ -11,11 +11,11 @@ $(function () {
     $('.lrc_content').niceScroll({
         cursorcolor: "#FFF",//#CC0071 光标颜色 
         cursoropacitymax: 0.5, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0 
-        touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备 
-        cursorwidth: "10px", //像素光标的宽度 
-        cursorborder: "0", //     游标边框css定义 
-        cursorborderradius: "5px",//以像素为光标边界半径 
-        autohidemode: false //是否隐藏滚动条 
+        touchbehavior: false, 
+        cursorwidth: "10px", 
+        cursorborder: "0", 
+        cursorborderradius: "5px",
+        autohidemode: false
     });
 
 })
