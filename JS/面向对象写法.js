d6742797ea19d53acf279e9d70db40f69d99935f
GitHpotato <hengxipeng@qq.com>
2019-09-01 00:23:53 +0800

    添加注释

--- JS/面向对象写法.js
+++ JS/面向对象写法.js
@@ -1,6 +1,18 @@
+/**
+ * create by Function in 2019/08/31
+ * 作用： 面向对象思想示例
+ * Zoo 构造函数
+ *  init    :初始化方法
+ *  IIFE    :函数写法（立即执行函数表达式）
+ * ********************************* *
+ * 使用示例：
+ * <button onclick="request()">发送请求</button>
+ * 
+ **/
 (function(){
     function Zoo(){
         return new Zoo.prototype.init();
+/*它的作用是让实例化出的对象改为init函数实例出的对象*/
     }
     Zoo.prototype={
         constructor:Zoo,
@@ -17,19 +29,19 @@
  * 我在构造函数Zoo的原型对象中创建一个init函数
  * 把这个函数自己的原型对象init.prototype
  * 改为Zoo的原型对象Zoo.prototype;
- * ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+ * ↓   ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓ 
  */
     Zoo.prototype.init.prototype=Zoo.prototype;
 /**
- * 这里dog相当于init函数实例出的对象
+ * 这里dog是通过init函数实例出的
+ * 因为init函数的原型init.prototype已经改为Zoo的原型
+ * dog.__proto__===init.prototype返回true
+ * 所以dog.say()就能访问它的原型init身上的属性或方法了
+ * ↓   ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓ 
 */
 var dog=new Zoo()
     dog.say();
-    /***
-     * dog.init();
-     * dog.say();
-     * 必须要先提前调用init方法
-     * 然后调用say才能取得数据
-     */
-
+/**
+ * 根据jquery的方法思想而来
+ */
 })();
